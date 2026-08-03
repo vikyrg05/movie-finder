6645dae462b56c6c60ba664bc884fc09f53ed475
vikyrg05 <vikyrg05@gmail.com>
2026-08-03 11:23:19 -0400

    Display complete movie details information

--- src/js/movie-details.js
+++ src/js/movie-details.js
@@ -7,19 +7,39 @@ const movieId = urlParams.get('id');
 async function displayMovieDetails() {
     const movie = await getMovieDetails(movieId);
 
+    const genres = movie.genres
+        .map((genre) => genre.name)
+        .join(', ');
+
+    console.log(movie);
+
     const movieDetails = document.querySelector('#movie-details');
 
     movieDetails.innerHTML = `
-        <h1>${movie.title}</h1>
+        <section class="movie-detail-card">
 
-        <img
-            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-            alt="${movie.title}"
-        >
-        
-        <p>${movie.overview}</p>
+            <img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                alt="${movie.title}"
+            >
 
-        <p>Release date: ${movie.release_date}</p>
+            <div class="movie-detail-info">
+
+                <h1>${movie.title}</h1>
+
+                <p>Rating: ${movie.vote_average}/10</p>
+
+                <p>Genres: ${genres}</p>
+
+                <p>Runtime: ${movie.runtime} minutes</p>
+
+                <p>Language: ${movie.original_language}</p>
+
+                <p>${movie.overview}</p>
+
+                <p>Release date: ${movie.release_date}</p>
+            </div>
+        </section>
     `;
 }
 
